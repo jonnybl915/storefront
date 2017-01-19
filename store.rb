95c247f5340a603caa12cc35547c067d599d3647
Jon Black <jblack@JolleCocoon.local>
2017-01-19 15:35:46 -0500

    finished number 7

--- store.rb
+++ store.rb
@@ -27,11 +27,8 @@ class Address < ActiveRecord::Base
 end
 
 
-#
-#
-#
-# How much would it cost to buy one of each tool?
-# How many total items did we sell?
+
+
 # How much was spent on books?
 # Simulate buying an item by inserting a User for yourself and an Order for that User.
 
@@ -84,3 +81,6 @@ puts tools.sum('price')
 print "inclusive: "
 puts tools2.sum('price')
 
+puts "Question: How many total items did we sell?"
+total_items = Order.sum('quantity')
+puts "Answer: #{total_items}"
